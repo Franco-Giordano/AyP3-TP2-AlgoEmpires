0e802b5223d1b84e35a77eb0d816b886572790c2
franco-giordano <francogior98@gmail.com>
2018-11-11 18:22:00 -0300

    Borrar RegionDeTresPorTres

--- src/algoempires/RegionDeTresPorTres.java
+++
@@ -1,8 +0,0 @@
-package algoempires;
-
-public class RegionDeTresPorTres extends Region {
-
-    public void RegionDeTresPorTres(Parcela[][] parcelasRecibidas) {
-        //TODO buscar alguna forma de forzar que el array sea solo de 3x3
-    }
-}
