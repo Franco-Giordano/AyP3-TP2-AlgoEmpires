934d6b686f2c439e03bf86b153d444dc2178c8c4
franco-giordano <francogior98@gmail.com>
2018-11-14 15:18:37 -0300

    Sacar atributo casilleroSupoDer en region

--- src/algoempires/tablero/Region.java
+++ src/algoempires/tablero/Region.java
@@ -5,23 +5,15 @@ import java.util.ArrayList;
 public class Region {
 
     private Casillero infIzquierdo;
-    private Casillero supDerecho;
     private int tamanioHorizontal;
     private int tamanioVertical;
 
     public Region(Casillero infIzquierdo, int tamanioHorizontal, int tamanioVertical) {
 
         this.infIzquierdo = infIzquierdo;
-        this.supDerecho = this.calcularExtremo(tamanioHorizontal, tamanioVertical);
         this.tamanioVertical = tamanioVertical;
         this.tamanioHorizontal = tamanioHorizontal;
 
-    }
-
-    private Casillero calcularExtremo(int tamanioHorizontal, int tamanioVertical) {
-
-        return new Casillero(this.infIzquierdo.getHorizontal() + tamanioHorizontal - 1,
-                this.infIzquierdo.getVertical()+ tamanioVertical - 1);
     }
 
 
