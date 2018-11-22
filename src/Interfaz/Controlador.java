bcb362c2ce20c6d3ab15d8fc5da7b41f73d37a18
federicoIsola <fedeisola2007@gmail.com>
2018-11-22 02:02:53 -0300

    Cambios insignificantes a la interfaz.

--- src/Interfaz/Controlador.java
+++ src/Interfaz/Controlador.java
@@ -3,38 +3,29 @@ package Interfaz;
 import algoempires.AlgoEmpires;
 import algoempires.entidad.Entidad;
 import algoempires.entidad.edificio.PlazaCentral;
-import algoempires.entidad.unidad.Unidad;
-import algoempires.entidad.unidad.guerrero.Arquero;
 import algoempires.entidad.unidad.utilero.Aldeano;
 import algoempires.tablero.Posicion;
-import algoempires.tablero.Terreno;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class Controlador {
 
+    private final int TAMANIO_CASILLERO = 50;
+    private final int VGAP = TAMANIO_CASILLERO / 10;
+    private final int HGAP = TAMANIO_CASILLERO / 10;
     @FXML
     GridPane pane;
-
     @FXML
     TextArea textArea;
-
     private AlgoEmpires juego;
-
-    private final int TAMANIO_CASILLERO = 50;
-
-    private final int VGAP = TAMANIO_CASILLERO / 10;
-
-    private final int HGAP = TAMANIO_CASILLERO / 10;
 
     public void initialize() {
 
@@ -44,13 +35,16 @@ public class Controlador {
 
         pane.setAlignment(Pos.CENTER);
 
+        textArea.centerShapeProperty();
+
+
     }
 
     public void crearCasilleros() {
 
-        for (int j = 0; j < juego.getTerreno().getTamVertical() ; j++) {
-            for (int i = 0; i < juego.getTerreno().getTamHorizontal() ; i++) {
-                pane.add(crearCasillero(i,j), i, juego.getTerreno().getTamVertical() - j);
+        for (int j = 0; j < juego.getTerreno().getTamVertical(); j++) {
+            for (int i = 0; i < juego.getTerreno().getTamHorizontal(); i++) {
+                pane.add(crearCasillero(i, j), i, juego.getTerreno().getTamVertical() - j);
             }
         }
     }
@@ -59,15 +53,15 @@ public class Controlador {
 
         Rectangle rectangulo = new Rectangle(TAMANIO_CASILLERO, TAMANIO_CASILLERO);
         //Text text = new Text();
-        Text posicion = new Text(String.format("%d,%d",i + 1 ,j +1 ));
+        Text posicion = new Text(String.format("%d,%d", i + 1, j + 1));
         StackPane stack = new StackPane();
-        stack.getChildren().addAll(rectangulo,posicion);
+        stack.getChildren().addAll(rectangulo, posicion);
 
-        if (juego.getTerreno().estaOcupada(new Posicion(i + 1 ,j + 1 ))) {
+        if (juego.getTerreno().estaOcupada(new Posicion(i + 1, j + 1))) {
             rectangulo.setStroke(Color.DARKRED);
             rectangulo.setFill(Color.DARKRED);
 
-        }else {
+        } else {
             rectangulo.setStroke(Color.GREEN);
             rectangulo.setFill(Color.GREEN);
         }
@@ -75,7 +69,7 @@ public class Controlador {
         rectangulo.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                Entidad entidad = juego.getTerreno().obtenerEntidadEnPosicion(new Posicion(i+1,j+1));
+                Entidad entidad = juego.getTerreno().obtenerEntidadEnPosicion(new Posicion(i + 1, j + 1));
                 if (entidad != null) {
                     if (entidad.getClass() == Aldeano.class) {
                         textArea.setText("Entidad: Aldeano (" + entidad + ")\nVida:" + entidad.getVida());
@@ -84,7 +78,7 @@ public class Controlador {
                     } else {
                         textArea.setText("Entidad: Castillo(" + entidad + ")\nVida:" + entidad.getVida());
                     }
-                }else {
+                } else {
                     textArea.setText("Este casillero está vacío!");
                 }
 
@@ -94,7 +88,7 @@ public class Controlador {
         return stack;
     }
 
-    public void setJuego(AlgoEmpires juego){
+    public void setJuego(AlgoEmpires juego) {
         this.juego = juego;
     }
 
--- src/Interfaz/Interfaz.java
+++ src/Interfaz/Interfaz.java
@@ -3,27 +3,26 @@ package Interfaz;
 import algoempires.AlgoEmpires;
 import algoempires.jugador.Jugador;
 import algoempires.tablero.Posicion;
-import algoempires.tablero.Terreno;
 import algoempires.tablero.direccion.DireccionAbajo;
 import algoempires.tablero.direccion.DireccionArriba;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
-
-import java.util.ArrayList;
 
 public class Interfaz extends Application {
 
     AlgoEmpires juego;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-        this.juego = new AlgoEmpires(13,16);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        this.juego = new AlgoEmpires(13, 16);
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("VistaInterfaz.fxml"));
 
@@ -37,23 +36,14 @@ public class Interfaz extends Application {
 
         primaryStage.setTitle("AlgoEmpires");
 
-        primaryStage.setMaximized(true);
-
-        primaryStage.setResizable(false);
-
         primaryStage.setScene(new Scene(root));
 
         primaryStage.show();
 
         Jugador[] jugadores = juego.getJugadores();
 
-        jugadores[0].moverUnidad(new Posicion(5,7),new DireccionArriba());
-        jugadores[1].moverUnidad(new Posicion(7,12), new DireccionAbajo());
+        jugadores[0].moverUnidad(new Posicion(5, 7), new DireccionArriba());
+        jugadores[1].moverUnidad(new Posicion(7, 12), new DireccionAbajo());
 
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
